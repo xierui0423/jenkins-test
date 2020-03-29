f249ef0a8fb742505b13406921f4b08b65d81ec5
Ray Xie <ray.xie@mullenloweprofero.com>
2020-03-29 22:47:48 +0800

    feat: test

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,10 @@
-import ADAHidden from './plugins/ada-hidden';
+import aDAHidden from './plugins/ada-hidden';
 import EqualHeight from './plugins/equal-height';
 import BgLoader from './plugins/background-loader';
 import Accordion from './accordion/accordion';
 import Header from './header/header';
 
-new ADAHidden('[data-js-ada-hidden]');
+new aDAHidden('[data-js-ada-hidden]');
 new EqualHeight('[data-js-equal-height]');
 new BgLoader('[data-js-bg-loader]');
 new Accordion('[data-js-accordion]');
