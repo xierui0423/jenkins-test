a3f768ca63535c7d014819aabf60d36a89ef2df9
Ray Xie <ray.xie@mullenloweprofero.com>
2020-03-29 23:08:10 +0800

    test: fail

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,11 @@
 import aDAHidden from './plugins/ada-hidden';
-import EqualHeight from './plugins/equal-height';
+import equalHeight from './plugins/equal-height';
 import BgLoader from './plugins/background-loader';
 import Accordion from './accordion/accordion';
 import Header from './header/header';
 
 new aDAHidden('[data-js-ada-hidden]');
-new EqualHeight('[data-js-equal-height]');
+new equalHeight('[data-js-equal-height]');
 new BgLoader('[data-js-bg-loader]');
 new Accordion('[data-js-accordion]');
 new Header();
